8fac0ad443aa8033ce604935f9186472d296fdb7
robert-uzar <robert.uzar1@gmail.com>
2014-01-16 20:10:26 +0100

    added parsing to xml document

--- src/matcher/Matcher.java
+++ src/matcher/Matcher.java
@@ -10,6 +10,19 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import vulndb.VulnerabilityReader;
 import vulndb.VulnerabilityTuple;
 import db.Database;
@@ -65,21 +78,42 @@ public class Matcher {
 	public void writeResultFile(String output_filepath) {
 		// TODO: take `matches` members and write them as XML file to `output_filepath`
 		
+		int id_num = 0;
+		
 		try{
-			PrintWriter writer = new PrintWriter(output_filepath, "UTF-8");
-			writer.close();
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 			
-			writer.println("<?XML version=\"1.0\"?>");
-			writer.println("<DOCUMENT>");
-			for (Match m : matches) {
-				writer.println("<ENTRY>"+m+"</ENTRY>");
+			// root element
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("document");
+			doc.appendChild(rootElement);
+			
+			for(Match m : matches){
+				id_num += 1;
+				
+				// entry element
+				Element entry = doc.createElement("entry");
+				entry.appendChild(doc.createTextNode(m.toString()));
+				entry.setAttribute("id", String.valueOf(id_num));
+				rootElement.appendChild(entry);
+				
+				// write the content into xml file
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(doc);
+				StreamResult result = new StreamResult(new File(output_filepath));
+				
+				transformer.transform(source, result);
+				System.out.println("File saved.");
 			}
-			writer.println("</DOCUMENT");
-		
-		} catch(IOException e){
-			e.printStackTrace();
+			
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
 		}
-		
+			
 	}
 	
 	/**
