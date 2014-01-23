c62e95f2374aabd742aefa32f77025ad761c696e
prokls <lukas.prokop@student.tugraz.at>
2014-01-23 19:15:59 +0100

    Major update.

--- src/db/Database.java
+++ src/db/Database.java
@@ -14,117 +14,101 @@ import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import matcher.SoftwareDependency;
+import matcher.SoftwareName;
+import matcher.VersionNumber;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
-
 public class Database {
 	public Connection conn = null;
-	public Database() {}
-	
-	private HashMap<String, String> readConfig()
-	{
+
+	public Database() {
+	}
+
+	private HashMap<String, String> readConfig() {
 		HashMap<String, String> config = new HashMap<String, String>();
-		try
-		{
+		try {
 			File fXmlFile = new File("config.xml");
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
-			
+
 			NodeList nList = doc.getElementsByTagName("database");
 			Node nNode = nList.item(0);
 			Element eElement = (Element) nNode;
-			
-			config.put("name", eElement.getElementsByTagName("name").item(0).getTextContent());
-			config.put("username", eElement.getElementsByTagName("username").item(0).getTextContent());
-			config.put("password", eElement.getElementsByTagName("password").item(0).getTextContent());
-			config.put("host", eElement.getElementsByTagName("host").item(0).getTextContent());
+
+			config.put("name", eElement.getElementsByTagName("name").item(0)
+					.getTextContent());
+			config.put("username", eElement.getElementsByTagName("username")
+					.item(0).getTextContent());
+			config.put("password", eElement.getElementsByTagName("password")
+					.item(0).getTextContent());
+			config.put("host", eElement.getElementsByTagName("host").item(0)
+					.getTextContent());
 		} catch (Exception e) {
 			System.err.println("Error with config.xml");
 		}
-		
+
 		return config;
 	}
-	
-	private String buildConnectionString(HashMap<String, String> config)
-	{
+
+	private String buildConnectionString(HashMap<String, String> config) {
 		String connectionString = new String();
 		connectionString = MessageFormat.format(
 				"jdbc:mysql://{0}/{1}?user={2}&password={3}",
-				config.get("host"), config.get("name"),
-				config.get("username"), config.get("password"));
-		
+				config.get("host"), config.get("name"), config.get("username"),
+				config.get("password"));
+
 		return connectionString;
 	}
-	
+
 	public void connect() {
 		HashMap<String, String> config = readConfig();
-		try 
-		{
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(buildConnectionString(config));
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
-		
+
 	}
-	
-	public static String[] splitVersion(String spec) {
-		Pattern re1 = Pattern.compile("((\\d+(\\.\\d+)+)([-+]\\w+)*)");
-		Pattern re2 = Pattern.compile("^[\\/_\t ]");
-		Pattern re3 = Pattern.compile( "[\\/_\t ]$");
 
-		Matcher m = re1.matcher(spec);
-		String version = null;
-		int v_pos = spec.length();
-
-		// retrieve version
-		if (m.find()) {
-			version = m.group(1);
-			v_pos = m.start();
-		}
-		
-		// retrieve software
-		String sw = spec.substring(0, v_pos);
-		while (re2.matcher(sw).find())
-			sw = sw.substring(1);
-		while (re3.matcher(sw).find())
-			sw = sw.substring(0, sw.length() - 1);
-
-		String[] tuple = {sw, version};
-		return tuple;
-	}
-	
 	public Set<ServerSoftwareTuple> getTuples() {
 		Statement sql = null;
 		HashSet<ServerSoftwareTuple> tuples = new HashSet<ServerSoftwareTuple>();
 		try {
 			sql = conn.createStatement();
-			ResultSet result = sql.executeQuery(
-					"SELECT * FROM Header INNER JOIN PageInfo " +
-					"ON Header.PAGE_ID = PageInfo.id " + 
-					"WHERE Header.name = 'X-Powered-By'");
-			
+			ResultSet result = sql
+					.executeQuery("SELECT * FROM Header INNER JOIN PageInfo "
+							+ "ON Header.PAGE_ID = PageInfo.id "
+							+ "WHERE (Header.name = 'X-Powered-By' OR "
+							+ "Header.name = 'META/Generator')");
+
 			while (result.next()) {
-				String powered[] = splitVersion(result.getString("Header.value"));
+				String powered[] = ServerSoftwareTuple.splitVersion(result
+						.getString("Header.value"));
 
+				// TODO: test isValid before and only create object, if enough data is valid
 				String host = result.getString("PageInfo.url");
-				String software = powered[0];
-				String version = powered[1];
+				SoftwareName software = new SoftwareName(powered[0]);
+				VersionNumber version = new VersionNumber(powered[1]);
+				SoftwareDependency dep = new SoftwareDependency("");
 
-				tuples.add(new ServerSoftwareTuple(host, software, version, ""));
+				ServerSoftwareTuple sst = new ServerSoftwareTuple(host,
+						software, version, dep);
+				System.out.println(sst.toString());
+				tuples.add(sst);
 			}
-			
+
 			if (sql != null)
 				sql.close();
 
@@ -133,7 +117,7 @@ public class Database {
 		}
 		return tuples;
 	}
-	
+
 	public void disconnect() {
 		try {
 			conn.close();
--- src/matcher/Match.java
+++ src/matcher/Match.java
@@ -17,22 +17,24 @@ public class Match {
 	}
 
 	/**
-	 * Retrieve some data about this vulnerability.
-	 * Return the value corresponding to `key`.
-	 * Uses the XML SAX parser for data retrieval.
+	 * Retrieve some data about this vulnerability. Return the value
+	 * corresponding to `key`. Uses the XML SAX parser for data retrieval.
 	 * 
-	 * @param vul_db_filepath  Filepath to vulnerability database (xml)
-	 * @param key  An attribute of the vulnerability (eg. summary)
-	 * @return String  the value corresponding to the key for this vul.
+	 * @param vul_db_filepath
+	 *            Filepath to vulnerability database (xml)
+	 * @param key
+	 *            An attribute of the vulnerability (eg. summary)
+	 * @return String the value corresponding to the key for this vul.
 	 */
 	public String retrieve(String vul_db_filepath, String cve, String key) {
 		// TODO: open `vul_db_filepath` database with XML SAX parser
 		// TODO: search for vulnerability with CVE `cve`
 		// TODO: close XML parser
-		// TODO: return value corresponding to `key` for the given vulnerability `cve`
+		// TODO: return value corresponding to `key` for the given vulnerability
+		// `cve`
 		return "TODO";
 	}
-	
+
 	public String toString() {
 		return MessageFormat.format("Match<{0}, {1}>", hostname, vul_id);
 	}
